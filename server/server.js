975dec72679a90aff5231481baea0996327de83e
Your Name <you@example.com>
2017-07-14 19:12:33 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
-const app = express();
+const socketIO = require("socket.io");
 
-app.use(express.static(path.join(__dirname, "../public")));
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("new user connected");
+
+  socket.on("disconnect", (socket) => {
+    console.log("user was disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`app server started on ${port}`);
 });
