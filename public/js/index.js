928156dbd162fbeafe8f1445235991565168c20a
Your Name <you@example.com>
2017-07-15 17:01:44 +0200

    add newMeassage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+
+socket.on("connect", function () {
+  console.log("connected to server");
+
+  socket.emit("createMessage", {
+    from: "jhon snow",
+    text: "winter is coming"
+  });
+});
+socket.on("disconnect", function () {
+  console.log("disconnected from server");
+});
+
+socket.on("newMessage", function (message) {
+  console.log("New Message", message);
+});
--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ app.use(express.static(path.join(__dirname, "../public")));
 io.on("connection", (socket) => {
   console.log("new user connected");
 
+  socket.emit("newMessage", {
+    from: "mike@example.com 2",
+    text: "hey, what is going on 2",
+    createdAt: 444123
+  });
+
+  socket.on("createMessage", (newMessage) => {
+    console.log("createMessage", newMessage);
+  });
+
   socket.on("disconnect", (socket) => {
     console.log("user was disconnected");
   });
