8e3971aed6fd1c95a0de51e759ce3503652ce65b
Your Name <you@example.com>
2017-07-16 14:19:58 +0200

    format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+var moment = require("moment");
+
+// var date = new Date();
+// var months = ["jan", "Feb"]
+// console.log(date.getMonth());
+
+// var date = moment();
+// date.add("1", "year");
+// console.log(date.format("MMM Do, YYYY"));
+
+var date = moment();
+console.log(date.format("k:mm a"));
