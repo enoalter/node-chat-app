131a93904c26d4f22f11dc74991e751ec62bc201
Your Name <you@example.com>
2017-07-16 18:41:51 +0200

    send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,10 +14,28 @@ function scrollToBottom() {
   }
 }
 socket.on("connect", function () {
-  console.log("connected to server");
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit("join", params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = "/";
+    } else {
+      console.log("no error");
+    }
+  });
 });
 socket.on("disconnect", function () {
   console.log("disconnected from server");
+});
+
+socket.on("updateUserList", function (users) {
+  var ol = jQuery("<ol></ol>");
+
+  users.forEach(function (user) {
+    ol.append(jQuery("<li></li>").text(user));
+  });
+  jQuery("#users").html(ol);
 });
 
 socket.on("newMessage", function (message) {
@@ -51,7 +69,6 @@ jQuery("#message-form").on("submit", function (e) {
 
   var messageTextBox = jQuery("[name=message]");
   socket.emit("createMessage", {
-    from: "User",
     text: messageTextBox.val()
   }, function () {
     messageTextBox.val("")
